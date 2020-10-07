bf33ddc76667eb4ce57255b6860e4fad95b2a667
Vladimir <vladimir160933@gmail.com>
2020-10-07 17:44:27 +0300

    added a build version

---
+++ build/precache-manifest.862e55eaab702228d4804cb396ef9624.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429d70758b21c21eebff8a2e64d93401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0bf1ab75e205a9432a1",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "816fe8b4fc103f4abc43",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f0bf1ab75e205a9432a1",
+    "url": "/static/js/2.83f0670d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.83f0670d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816fe8b4fc103f4abc43",
+    "url": "/static/js/main.a09f8cf7.chunk.js"
+  },
+  {
+    "revision": "b75c6a0cd71a4f79e71a",
+    "url": "/static/js/runtime-main.287b25f4.js"
+  },
+  {
+    "revision": "ebc957f68c6bd3e25bf3664f86ed6b05",
+    "url": "/static/media/forest_1.ebc957f6.jpg"
+  },
+  {
+    "revision": "5492aeb1decaafd7e201026306020890",
+    "url": "/static/media/forest_2.5492aeb1.jpg"
+  },
+  {
+    "revision": "4d7678ae3122eb3bc2d4c0e628613bc1",
+    "url": "/static/media/forest_3.4d7678ae.jpg"
+  },
+  {
+    "revision": "bc6ef7917994cc6ff42212f890dc812e",
+    "url": "/static/media/img_1.bc6ef791.jpg"
+  },
+  {
+    "revision": "14a75fc55e1d80495cf1cc7a6f92eca0",
+    "url": "/static/media/img_2.14a75fc5.jpeg"
+  },
+  {
+    "revision": "4bc373117165b05762cb4be0d52f3244",
+    "url": "/static/media/img_3.4bc37311.jpg"
+  },
+  {
+    "revision": "e6f29a9ccf7877e6ae4f73d56f74b4c4",
+    "url": "/static/media/img_4.e6f29a9c.png"
+  },
+  {
+    "revision": "9e1f58ba5af0134cf142812c9a021d88",
+    "url": "/static/media/img_5.9e1f58ba.jpg"
+  }
+]);
